40f5d08b68cfd6dc8917388a7a19db1b6b5e9a3a
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-09 12:25:12 +0200

    update package setup details

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-results",
+    name="py-results-prottonicfusion",
     version="0.0.1",
     author="prOttonicFusion",
     author_email="otto.lindblom@gmail.com",
