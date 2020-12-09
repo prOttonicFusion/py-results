a95477d37cf61e9ed005c4fce82e68e76522eb2c
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-09 12:23:59 +0200

    add package setup details

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-results",
+    version="0.0.1",
+    author="prOttonicFusion",
+    author_email="otto.lindblom@gmail.com",
+    description="A result wrapping object for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
