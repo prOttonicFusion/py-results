3ab72267d0466b1b13620cd9ee4e7d4fcc0443fb
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-10 11:17:06 +0200

    add is_ok & is_err functions

--- py_results/result.py
+++ py_results/result.py
@@ -1,22 +1,22 @@
 class Result():
-    """
-    Stores the result of an operation. 
-    """
+    """Stores the result of an operation"""
 
     def __init__(self):
         self.ok_value = None
         self.err_value = None
 
     def ok(self, ok_value):
-        """
-        The result is a success
-        """
+        """The result is a success, store the relevant return value"""
         self.ok_value = ok_value
         self.err_value = None
 
     def err(self, err_value):
-        """
-        The result is a fail
-        """
+        """The result is a fail, store the relevant return value"""
         self.err_value = err_value
         self.ok_value = None
+
+    def is_ok(self):
+        return self.ok_value is not None
+
+    def is_err(self):
+        return self.err_value is not None
