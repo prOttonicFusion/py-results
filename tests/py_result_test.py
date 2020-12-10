e0ea47f94f9234cdb97c8a651a6c72ea95f7bf28
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2020-12-10 11:55:40 +0200

    create basic unit tests

--- tests/py_result_test.py
+++ tests/py_result_test.py
@@ -1,7 +1,42 @@
 from py_results import result
 
-def test_sampleTest():
-    '''
-    Sample test
-    '''
-    assert 1 == 1, 'Something went wrong'
+
+def test_createEmptyResult():
+    '''Blank result object'''
+    blank_result = result.Result()
+    assert blank_result.is_ok() == False and blank_result.is_err(
+    ) == False, 'should be neither ok nor err'
+
+
+def test_createSuccessResult():
+    '''Result object with success status'''
+    ok_result = result.Result()
+    ok_result.ok("Success!")
+    assert ok_result.is_ok() == True, 'is_ok() should return true'
+    assert ok_result.is_err() == False, 'is_err() should return false'
+
+
+def test_createFailResult():
+    '''Result object with fail status'''
+    fail_result = result.Result()
+    fail_result.err("Epic fail!")
+    assert fail_result.is_ok() == False, 'is_ok() should return false'
+    assert fail_result.is_err() == True, 'is_err() should return true'
+
+
+def test_overrideSuccessResult():
+    '''Override success with fail result'''
+    fail_result = result.Result()
+    fail_result.ok("Success!")
+    fail_result.err("Uh, no it was a fail...")
+    assert fail_result.is_ok() == False, 'is_ok() should return false'
+    assert fail_result.is_err() == True, 'is_err() should return true'
+
+
+def test_overrideFailResult():
+    '''Override fail with success result'''
+    fail_result = result.Result()
+    fail_result.err("Fail :(")
+    fail_result.ok("No, a success!")
+    assert fail_result.is_ok() == True, 'is_ok() should return true'
+    assert fail_result.is_err() == False, 'is_err() should return false'
