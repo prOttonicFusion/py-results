b11161804fb8d4835df6fd56037c8611ae21998f
Otto Lindblom <61689180+prOttonicFusion@users.noreply.github.com>
2024-04-18 22:37:39 +0300

    add type hints

--- py_results/result.py
+++ py_results/result.py
@@ -1,7 +1,12 @@
-class Result:
-    def __init__(self):
-        self.ok_value = None
-        self.err_message = None
+from typing import TypeVar, Generic
+
+T = TypeVar("T")
+
+
+class Result(Generic[T]):
+    def __init__(self, ok_value, err_message: str | None = None):
+        self.ok_value = ok_value
+        self.err_message = err_message
 
     def is_ok(self):
         return self.ok_value is not None
@@ -9,21 +14,28 @@ class Result:
     def is_err(self):
         return self.err_message is not None
 
-    def unwrap(self):
+    def unwrap(self) -> T | str:
         """Unwrap the contained data, returning either the success value or the error message"""
         if self.err_message != None:
             return self.err_message
-        else:
-            return self.ok_value
+
+        return self.ok_value
 
 
-class Ok(Result):
-    def __init__(self, value):
-        super().__init__()
-        self.ok_value = value
+class Ok(Result, Generic[T]):
+    def __init__(self, value: T):
+        super().__init__(ok_value=value)
+
+    def unwrap(self) -> T:
+        """Unwrap the contained value"""
+        return self.ok_value
 
 
 class Err(Result):
-    def __init__(self, message):
-        super().__init__()
+    def __init__(self, message: str):
+        super().__init__(ok_value=None)
         self.err_message = message
+
+    def unwrap(self) -> str:
+        """Unwrap the error message"""
+        return self.err_message
--- tests/test_py_results.py
+++ tests/test_py_results.py
@@ -3,10 +3,6 @@ from py_results import Result, Ok, Err
 
 
 class TestCreateResult(unittest.TestCase):
-    def test_emptyResultShouldNotBeOk(self):
-        blank_result = Result()
-        self.assertEqual(blank_result.is_ok(), False)
-
     def test_successResultShouldBeOk(self):
         ok_result = Ok("Success!")
         self.assertEqual(ok_result.is_ok(), True)
@@ -19,8 +15,8 @@ class TestCreateResult(unittest.TestCase):
 
 
 class TestUnwrapResult(unittest.TestCase):
-    def test_shouldUnwrapEmptyResultAsNone(self):
-        blank_result = Result()
+    def test_shouldSupportNoneAsSuccessValue(self):
+        blank_result = Ok(None)
         self.assertEqual(blank_result.unwrap(), None)
 
     def test_shouldUnwrapSuccessResultAsValue(self):
@@ -30,3 +26,25 @@ class TestUnwrapResult(unittest.TestCase):
     def test_shouldUnwrapFailResultAsErrorMessage(self):
         fail_result = Err("Something went wrong...")
         self.assertEqual(fail_result.unwrap(), "Something went wrong...")
+
+
+def int_divide(a: int, b: int) -> Result[int]:
+    if b == 0:
+        return Err("Division by zero")
+    return Ok(a // b)
+
+
+class TestUseCaseIntDivide(unittest.TestCase):
+    def test_divideByZeroShouldReturnError(self):
+        result = int_divide(10, 0)
+        self.assertIsInstance(result, Err)
+        self.assertEqual(result.is_ok(), False)
+        self.assertEqual(result.is_err(), True)
+        self.assertEqual(result.unwrap(), "Division by zero")
+
+    def test_divideByNonZeroShouldReturnOk(self):
+        result = int_divide(10, 2)
+        self.assertIsInstance(result, Ok)
+        self.assertEqual(result.is_ok(), True)
+        self.assertEqual(result.is_err(), False)
+        self.assertEqual(result.unwrap(), 5)
